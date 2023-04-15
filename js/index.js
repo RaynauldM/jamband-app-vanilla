763991c44faf18751e842cb9a6e9c7fc52045759
RaynauldM <raynauldminkema@gmail.com>
2023-04-15 15:09:30 +0200

    background, aanmeldlink aangemaakt, mobile first gegaan

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 const test = document.querySelector("h3");
+const btns = document.getElementsByClassName("btns");
+
+function handleClick(event) {
+  const { id } = event.target;
+  switch (id) {
+    case "setBtn":
+      document.location = "../html/setlijstPage.html";
+      break;
+    case "sendBtn":
+      document.location = "../html/aanmeldPage.html";
+  }
+}
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", handleClick);
+}
